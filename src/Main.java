45b6ddd028e9b4a5532b88dc9fe73167aef5c6c6
Rupali Choudhari <rchoudhari2407@gmail.com>
2022-06-30 20:10:39 +0530

    Commited

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.singleton.Singleton;
+
+class  Main{
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton);
+        System.out.println(singleton1);
+        System.out.println(singleton2);
+    
+    }
+}
